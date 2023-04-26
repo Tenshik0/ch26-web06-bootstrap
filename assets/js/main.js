e0857efc4df09013227c107d3dd6a7200fc31bcf
Tenshik0 <tenshik0.ale@gmail.com>
2023-04-25 20:31:52 -0600

    JS
    Se vio como cambiar el color del texto y otras cosas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,24 @@
 console.log("Hola desde un Script externo");
 
-function changeColorToRed(){
-    const refName = document.getElementById("Name");
+function changeColorTo(color){
+    const refName = getReferenceFullName();
 
-    refName.style.color = "red";
+    refName.style.color = color;
+}
+
+function getReferenceFullName(){
+    return document.getElementById("fullname");
+}
+
+function changeColorParagraph(color){
+    const refTxtGreen = document.getElementById("verde");
+    const refTxtBlue = document.getElementById("azul");
+    const refTxtYellow = document.getElementById("amarillo");
+
+    refTxtGreen.style.color = color;
+}
+
+function resetColors(){
+    const colorCollection
+
 }
